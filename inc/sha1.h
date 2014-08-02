d12ac8de5a78540416fa4bb9c424ae1a9b55a227
Paolo Carrara <paolocarraratux@gmail.com>
2014-08-02 17:21:11 -0300

    Move as funções que estavam em sha.c para sha1.c, pois elas melhor se encaixam desse modo. Refatoração de modo a deixar a execução do código mais rápido (eu acho..). Moves the functions from sha.c to sha1.c, because they better fit this way. Refactorization in a way that the code execution get faster (I think..).

--- inc/sha1.h
+++ inc/sha1.h
@@ -6,11 +6,11 @@
 #include "sha.h"
 
 #ifdef __LP64__
-__attribute__((flatten, optimize("O3")))
-uint32_t *sha1 (uint8_t **const, uint64_t);
+__attribute__((flatten, optimize("Ofast","-funroll-loops")))
+uint32_t *sha1 (uint8_t *, uint64_t);
 #else
-__attribute__((flatten, optimize("O3")))
-uint32_t *sha1 (uint8_t **const, uint32_t);
+__attribute__((flatten, optimize("Ofast","-funroll-loops")))
+uint32_t *sha1 (uint8_t *, uint32_t);
 #endif
 
 #endif
--- src/sha1.c
+++ src/sha1.c
@@ -5,43 +5,70 @@
 #define Hsize		0x05
 #define ABCDEsize	0x05
 
-__attribute__((always_inline, optimize("O3"), nonnull, pure, hot))
-static inline void schedule	(uint32_t *const, const uint8_t *const);
-
-__attribute__((always_inline, optimize("O3"), warn_unused_result, hot))
-static inline uint32_t f	(const uint32_t, const uint32_t, const uint32_t, const uint32_t);
+#ifdef ARCHITECTURE64
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t *padd512	(uint8_t *, uint64_t *const);
+#else
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t *padd512	(uint8_t *, uint32_t *const);
+#endif
 
 #ifdef ARCHITECTURE64
-__attribute__((flatten, optimize("O3","-funroll-loops")))
-uint32_t *sha1 (uint8_t **const M, uint64_t N)
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t **prsng512 (uint8_t * const M, const uint64_t sz, uint64_t *const N);
 #else
-__attribute__((flatten, optimize("O3","-funroll-loops")))
-uint32_t *sha1 (uint8_t **const M, uint32_t N)
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t **prsng512 (uint8_t * const M, const uint32_t sz, uint32_t *const N);
+#endif
+
+__attribute__((always_inline, optimize("Ofast"), nonnull, pure, hot))
+static inline void schedule	(uint32_t *const, const uint8_t *const);
+
+__attribute__((always_inline, optimize("Ofast"), warn_unused_result, hot))
+static inline uint32_t f	(const uint32_t, const uint32_t, const uint32_t, const uint32_t);
+
+
+#ifdef ARCHITECTURE64
+__attribute__((flatten, optimize("Ofast","-funroll-loops")))
+uint32_t *sha1 (uint8_t *M, uint64_t sz)
+#else
+__attribute__((flatten, optimize("Ofast","-funroll-loops")))
+uint32_t *sha1 (uint8_t *M, uint32_t sz)
 #endif
 {
 	#ifdef ARCHITECTURE64
 	uint64_t i;
+	uint64_t N;
 	#else
 	uint32_t i;
+	uint32_t N;
 	#endif
 	uint8_t j;
-
+	uint8_t **m;
+	
 	/*Variables of the specifications*/
 	uint32_t T;
 	uint32_t *const H = malloc (Hsize*sizeof (uint32_t));
 	uint32_t abcde [ABCDEsize];
-	uint32_t K [Ksize] = {[0 ... 19] = 0x5a827999, [20 ... 39] = 0x6ed9eba1, [40 ... 59] = 0x8f1bbcdc, [60 ... 79] = 0xca62c1d6};
 	uint32_t W [Wsize];
+	uint32_t K [Ksize] = {	[0 ... 19] = 0x5a827999, 
+				[20 ... 39] = 0x6ed9eba1, 
+				[40 ... 59] = 0x8f1bbcdc, 
+				[60 ... 79] = 0xca62c1d6 };
 
 	H[0] = 0x67452301;
 	H[1] = 0xefcdab89;
 	H[2] = 0x98badcfe;
 	H[3] = 0x10325476;
 	H[4] = 0xc3d2e1f0;
+	
+	/* preprocessing */
+	M = padd512 (M, &sz);
+	m = prsng512 (M, sz, &N);
 
 	for (i = 0; i < N; i++) {
 
-		schedule (W, M[i]);
+		schedule (W, m[i]);
 
 		for (j = 0; j < 5; j++) {
 			abcde[j] = H[j];
@@ -60,12 +87,64 @@ uint32_t *sha1 (uint8_t **const M, uint32_t N)
 			H[j] += abcde[j];
 		}
 	}
+	
+	free (m);
+	free (M);
 
 	return H;
 }
+#ifdef ARCHITECTURE64
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t *padd512 (uint8_t *M, uint64_t *const sz)
+#else
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t *padd512 (uint8_t *M, uint32_t *const sz)
+#endif
+{
+	uint8_t r, g;
 
-__attribute__((always_inline, optimize("O3"), nonnull, pure, hot))
-static void schedule (uint32_t *const W, const uint8_t *const M)
+	r = ((*sz)+MIN_REALL_SZ)%SHA_512_BLOCK;
+	g = SHA_512_BLOCK - r;
+
+	M = realloc (M, (*sz)+g+MIN_REALL_SZ);
+
+	M[(*sz)] = MIN_PADD_VLE;
+	memset (M+(*sz)+1, 0, g+4);
+	M[(*sz)+g+8] = (*sz)*8 >> 0;
+	M[(*sz)+g+7] = (*sz)*8 >> 8;
+	M[(*sz)+g+6] = (*sz)*8 >> 16;
+	M[(*sz)+g+5] = (*sz)*8 >> 24;
+	(*sz) += g + MIN_REALL_SZ;
+
+	return M;
+}
+
+#ifdef ARCHITECTURE64
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t **prsng512 (uint8_t * const M, const uint64_t sz, uint64_t *const N)
+#else
+__attribute__((always_inline, nonnull, warn_unused_result, optimize("Ofast")))
+static inline uint8_t **prsng512 (uint8_t * const M, const uint32_t sz, uint32_t *const N)
+#endif
+{
+	uint8_t **blocks = NULL;
+	#ifdef ARCHITECTURE64
+	uint64_t i;
+	#else
+	uint32_t i;
+	#endif
+
+	(*N) = sz/SHA_512_BLOCK;
+	blocks = malloc ((*N)*sizeof(uint8_t *));
+
+	for (i = 0; i < (*N); i++)
+			blocks[i] = M+(i*SHA_512_BLOCK);
+
+	return blocks;
+}
+
+__attribute__((always_inline, optimize("Ofast"), nonnull, pure, hot))
+static inline void schedule (uint32_t *const W, const uint8_t *const M)
 {
 	uint8_t i;
 
@@ -87,8 +166,8 @@ static void schedule (uint32_t *const W, const uint8_t *const M)
 	}
 }
 
-__attribute__((always_inline, optimize("O3"), warn_unused_result, hot))
-static uint32_t f (const uint32_t b, const uint32_t c, const uint32_t d, const uint32_t t)
+__attribute__((always_inline, optimize("Ofast"), warn_unused_result, hot))
+static inline uint32_t f (const uint32_t b, const uint32_t c, const uint32_t d, const uint32_t t)
 {
 	if (t >= 60) {
 		return PA (b, c, d);
